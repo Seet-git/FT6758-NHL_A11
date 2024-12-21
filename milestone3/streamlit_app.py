9af85f9cb5a7fc45a0f7e902424ac3759813b8be
HP-SPECTRE-BOBY\franc <franckatongfor@yahoo.fr>
2024-12-21 10:52:13 -0500

    showing model outpout on streamlit app

--- milestone3/streamlit_app.py
+++ milestone3/streamlit_app.py
@@ -4,13 +4,14 @@ import numpy as np
 import sys
 import os
 
-# from milestone3.ift6758.ift6758.client import *
+from ift6758.ift6758.client import serving_client, game_client
 
 # Ajouter le chemin du dossier `client` au `sys.path`
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'ift6758', 'ift6758', 'client')))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'ift6758', 'ift6758', 'client')))
 
 # Importer le fichier `serving_client`
-import serving_client
+#import serving_client
+#import game_client
 
 """
 General template for your streamlit app. 
@@ -18,13 +19,37 @@ Feel free to experiment with layout and adding functionality!
 Just make sure that the required functionality is included as well
 """
 
-st.title("Hockey visualisation app")
-
 WANDB_PROJECT_NAME = "IFT6758.2024-A11"
 WANDB_TEAM_NAME = "youry-macius-universite-de-montreal"
 
 actual_model = "LogisticRegression_Distance"
 models_list = ["LogisticRegression_Distance", "LogisticRegression_Distance_Angle"]
+
+BASE_URL = "https://api-web.nhle.com/v1/gamecenter"
+PROVIDER_IP_ADDRESS = "127.0.0.1"
+PROVIDER_SERVICE_URL = f"http://{PROVIDER_IP_ADDRESS}:5000"
+
+
+def fetch_game_data(game_id):
+    # Données simulées
+    game_info = {
+        'Game ID': game_id,
+        'Date': '2024-12-21',
+        'Teams': 'Team A vs Team B',
+        'Location': 'Stadium XYZ'
+    }
+
+    client = game_client.GameClient(base_url=BASE_URL, model_service_url=PROVIDER_SERVICE_URL)
+
+    data = client.process_game(game_id)
+
+    data = data.rename(columns={'goal_probs': 'Model output'})
+
+    data.index = [f"event {i}" for i in range(len(data['Model output']))]
+
+    return game_info, data
+
+st.title("Hockey visualisation app")
 
 with st.sidebar:
     # TODO: Add input for the sidebar
@@ -36,29 +61,21 @@ with st.sidebar:
     # Ajouter une liste de sélection à la sidebar
     selected_workspace = st.sidebar.selectbox( "Workspace", [workspace])
 
-    # Afficher la sélection
-    st.write(f"Vous avez sélectionné : {selected_workspace}")
-
     # Créer une sidebar
     st.sidebar.title("Menu")
 
     # Ajouter une liste de sélection à la sidebar
     selected_model = st.sidebar.selectbox("Model", models_list)
 
-    st.write(f"Vous avez sélectionné : {selected_model}")
+    # st.write(f"Vous avez sélectionné : {selected_model}")
 
     versions_list = ["latest"]
 
     # Ajouter une liste de sélection à la sidebar
     selected_version = st.sidebar.selectbox("Version", versions_list)
 
-    st.write(f"Vous avez sélectionné : {selected_version}")
-
     if st.sidebar.button('Get model'):
-        # have_it = animal.lower() in animal_shelter
-        # 'We have that animal!' if have_it else 'We don\'t have that animal.'
-
-        client = serving_client.ServingClient(ip="127.0.0.1")
+        client = serving_client.ServingClient(ip=PROVIDER_IP_ADDRESS)
 
         result = client.download_registry_model(workspace=selected_workspace, model=selected_model, version=selected_version)
 
@@ -67,16 +84,25 @@ with st.sidebar:
         else:
             st.error("Une érreur est survenue lors de la récupération du modèle.")
 
-    st.write(f"Modèle actif : {actual_model}")
+    st.write(f"Modèle actuel : {actual_model}")
+
 
 with st.container():
-    # TODO: Add Game ID input
-    pass
+    # Champ de saisie pour le Game ID
+    game_id = st.text_input("Game ID", "2021020329")
 
-with st.container():
-    # TODO: Add Game info and predictions
-    pass
+    # Bouton pour ping le jeu
+    if st.button("Ping game"):
+        # Récupérer les données du jeu et les informations du jeu
+        game_info, game_data = fetch_game_data(game_id)
 
-with st.container():
-    # TODO: Add data used for predictions
-    pass
+        # Afficher les informations du jeu
+        st.write("Game Information")
+        for key, value in game_info.items():
+            st.write(f"{key}: {value}")
+
+        # Afficher les données du jeu
+        st.header("Data used for predictions (and predictions)")
+
+        st.dataframe(game_data)
+
